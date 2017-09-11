80ad7ba245a72cded73a59f0bb38843eeb0e52de
Davide Ramondetti <github@ramondettidavide.com>
2017-09-11 21:16:58 +0000

    Update index.php

--- IsOnlineOP/index.php
+++ IsOnlineOP/index.php
@@ -1,2 +1,23 @@
 <?php
 session_start();
+include("config.php");
+
+if($_POST['submit']){
+  $websiteURL = $_POST['websiteURL'];
+  $websiteProtocol = $_POST['websiteProtocol'];
+  
+  $_SESSION['websiteURL'] = $websiteURL;
+  $_SESSION['websiteProtocol'] = $websiteProtocol;
+  
+  getCheck();
+  
+  $url = $_GET['url'];
+$host = $url; 
+$port = 80;  
+$waitTimeoutInSeconds = 1; 
+if($fp = fsockopen($host,$port,$errCode,$errStr,$waitTimeoutInSeconds)){   
+   $result = "ok"; 
+} else {
+   $result = "no";
+} 
+fclose($fp);
